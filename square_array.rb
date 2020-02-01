77e4a4ecae06c9b2b587e6c5ed8b29810cc2da66
TShipley <taneymail@gmail.com>
2020-02-01 05:41:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  square_array => [1,2,3]
-  
-array.each do |num|
-  new_array push (num ** 2)
+  # your code here
+  square_numbers=[]
+  array.each do |number|
+    square_numbers.push(number * number)
 end
-
-return new_array
+return square_numbers
+end 
